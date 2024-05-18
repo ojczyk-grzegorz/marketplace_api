af9543cc3616bf37a0ed05566537fd4b64e33955
Grzegorz Ojczyk <ojczyk.grzegorz@gmail.com>
2024-05-18 18:31:51 +0200

    Added categories items return endpoint

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 
+from db.db import get_items
+from data_models.categories import Categories
+
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", description="Home page")
 def home():
-    return {"message": "Welcome to my marketpalce ;)"}
+    return {"message": "Welcome to my marketplace ;)"}
+
+
+@app.get("/{category}")
+def get_category(category: Categories):
+    return get_items(category)
