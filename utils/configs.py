c5ec533e827da9e860b3f6a9bc24e8aa8c1eb678
Grzegorz Ojczyk <ojczyk.grzegorz@gmail.com>
2025-07-06 22:43:37 +0200

    Added configs

---
+++ utils/configs.py
@@ -0,0 +1,45 @@
+import json
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings
+
+
+class SettingsDBTable(BaseSettings):
+    name: str
+
+
+class SettingsDBTables(BaseSettings):
+    users: SettingsDBTable
+    items: SettingsDBTable
+    transactions: SettingsDBTable
+    logs_request: SettingsDBTable
+    logs_query: SettingsDBTable
+
+
+class SettingsAuth(BaseSettings):
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+
+class SettingsDB(BaseSettings):
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+    tables: SettingsDBTables
+
+
+class Settings(BaseSettings):
+    app_name: str
+    database: SettingsDB
+    auth: SettingsAuth
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    with open("configs.json", "r") as f:
+        settings_data = json.load(f)
+        settings = Settings.model_validate(settings_data)
+    return settings
